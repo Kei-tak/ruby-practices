a5b761ed834c90e86349fc230e91649485beb2de
Kei-tak <tkhs0139@gmail.com>
2022-04-16 20:38:20 +0900

    fizzbuzz課題

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,13 @@
 sum = 0
 20.times do
   sum += 1
-  if sum/15.0 == 1
+  if sum%15.0 == 0
     puts "FizzBuzz"
     next
-  elsif sum/3.0 == (1.0) .. (20.0)
+  elsif sum%3.0 == 0
     puts "Fizz"
     next
-  elsif sum/5.0 == 1||sum/5.0 == 2||sum/5.0 == 3||sum/5.0 == 4
+  elsif sum%5.0 == 0
     puts "Buzz"
     next
   end
