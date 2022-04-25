f7a11661caf791892c3422dee4ad00bec5b6294c
Kei-tak <tkhs0139@gmail.com>
2022-04-26 02:46:43 +0900

    calendar 修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,54 +1,48 @@
-require "date"
+require 'date'
 require 'optparse'
 
 today = Date.today
 
 params = ARGV.getopts("m:y:", "month:", "year:")
-month = params["m"].to_i
-year = params["y"].to_i
-
-if params["m"].nil? && params["month"].nil?
+if params["m"]
+  month = params["m"].to_i
+elsif params["month"]
+  month = params["month"].to_i
+else
   month = today.month
 end
 
-if params["y"].nil? && params["year"].nil?
+if params["y"]
+  year = params["y"].to_i
+elsif params["year"]
+  year = params["year"].to_i
+else
   year = today.year
 end
 
 day_first = Date.new(year, month,  1)
 day_last = Date.new(year, month, -1)
 
-days = day_first..day_last
+days_week = ["日", "月", "火", "水", "木", "金", "土"]
 
-days_week = [" 日", "月", "火", "水", "木", "金", "土"]
+puts "      #{month}月 #{year}"
 
-print("      ",month,"月"," ",year)
-puts
-
-puts days_week.join(" ")
-
-if day_first.monday?
-  print "   "
-elsif day_first.tuesday?
-  print "      "
-elsif day_first.wednesday?
-  print "         "
-elsif day_first.thursday?
-  print "            "
-elsif day_first.friday?
-  print " 　　　　　　  "
-elsif day_first.saturday?
-  print "                  "
+if day_first.sunday?
+  print (" #{days_week.join(" ")}")
+else
+  puts (" #{days_week.join(" ")}")
 end
 
-days.each do |dd|
-  if dd.sunday?
-    puts
-  end
-  if dd == today
-    print " \e[30;47;5m#{dd.mday}\e[0m".rjust(3)
+day_first.wday.times {print "   "}
+
+days = day_first..day_last
+
+days.each do |day|
+  puts  if day.sunday?
+  if day == today
+    print " \e[30;47;5m#{day.mday}\e[0m".rjust(3)
     next
   end
-print "#{dd.mday}".rjust(3)
+  print "#{day.mday}".rjust(3)
 end
 puts
